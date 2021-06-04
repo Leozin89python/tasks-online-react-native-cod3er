b75ae513138420eb5878bba5faf8d1f6ccf2aa87
Leozin89Python <leozin069@gmail.com>
2021-06-04 20:27:28 -0300

    tasks

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 8080
+const db = require('./config/db')
+const consign = require('consign')
+
+
+consign()
+        .then('./config/middlewares.js')
+        .then('./api')
+        .then('./config/routes.js')
+        .into(app)
+
+
+app.db = db
+
+app.listen(port,()=>{
+    console.log(`port:${port}`)
+})
+
---
+++ db/db.sql
@@ -0,0 +1,18 @@
+								/*postgres*/
+								
+								
+								
+
+CREATE DATABASE tasks;
+
+\l
+\c tasks
+
+/*or */
+
+USE tasks;
+
+\dt
+
+SELECT * FROM users;
+SELECT * FROM tasks;
